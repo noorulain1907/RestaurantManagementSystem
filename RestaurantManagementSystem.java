c21c68cd696aaf7c59da80dde63aaadebf8074e3
noorulain1907 <151053022+noorulain1907@users.noreply.github.com>
2024-01-21 20:46:53 +0500

    Add files via upload

---
+++ RestaurantManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantmanagementsystem;
+
+/**
+ *
+ * @author Muhammad Ahsan
+ */
+public class RestaurantManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
